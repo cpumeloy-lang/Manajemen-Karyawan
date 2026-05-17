0882d1e464e589a2ed3acc33abf3bd903813e376
AuraCore Labs <cpumeloy@gmail.com>
2026-05-17 20:33:42 +0800

    fix: CORS allow PUT & DELETE methods, fix 405 error saat update karyawan

--- server.js
+++ server.js
@@ -277,7 +277,7 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (origin && (ALLOWED_ORIGINS.length === 0 || ALLOWED_ORIGINS.includes(origin))) {
     res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Max-Age', '86400');
   }
